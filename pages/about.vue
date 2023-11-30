46d24f9ebd17aae12cf6678fa2355672ac05c96a
pica69co <oscarwarrieta@gmail.com>
2023-11-30 17:06:26 -0500

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>About dad Jokes Page</h1>
+    <p>This is an app that displays corny dad jokes</p>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: "About Us",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>Welcome to the best corny dad jokes app on the web</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
